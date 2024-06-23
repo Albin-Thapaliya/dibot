822ee431fae0b53721a556622af0c183173a6a4d
Albin Thapaliya <wingsbroken04@gmail.com>
2024-06-23 13:36:14 +0000

    fix return type

--- src/routes/Clan.js
+++ src/routes/Clan.js
@@ -40,10 +40,11 @@ Router.post("/createClan", (req, res) => {
       PaymentMembers: 50,
     },
   }).save((err, saved) => {
-    if (err) return res.status(404).json(err);
+    if (err)
+      return res.status(404).json({ Error: "Not able to save", Success: null });
 
     console.log("New clan created and received!");
-    return res.status(201).json("Success");
+    return res.status(201).json({ Error: null, Success: saved.toObject() });
   });
 });
 
@@ -69,10 +70,11 @@ Router.post("/inviteMember", (req, res) => {
     },
     RoleId: inviteResult.RoleId,
   }).save((err, saved) => {
-    if (err) return res.status(404).json(err);
+    if (err)
+      return res.status(404).json({ Error: "Not able to save", Success: null });
 
     console.log("New invite created");
-    return res.status(201).json("Success");
+    return res.status(201).json({ Error: null, Success: playfabId });
   });
 });
 
@@ -89,10 +91,11 @@ Router.post("/addApp", (req, res) => {
     Expires: applicationResult.Expires,
     Group: applicationResult.Group,
   }).save((err, saved) => {
-    if (err) return res.status(404).json(err);
+    if (err)
+      return res.status(404).json({ Error: "Not able to save", Success: null });
 
     console.log("Created app");
-    return res.status(201).json("Success");
+    return res.status(201).json({ Error: null, Success: playfabId });
   });
 });
 
@@ -105,20 +108,28 @@ Router.post("/acceptInvite", (req, res) => {
     if (err) return console.error(err);
 
     if (!clan)
-      return res.status(404).json({ Error: "No clan with that Id found" });
+      return res
+        .status(404)
+        .json({ Error: "This clan doesn't exist", Success: null });
 
     InviteSchema.find({ Group: groupData }, (err, invites) => {
       if (err)
-        return res.status(404).json({ Error: "No invites to that clan found" });
+        return res
+          .status(404)
+          .json({ Error: "No clan with that Id found", Success: null });
 
       if (invites.length <= 0)
-        return res.status(404).json({ Error: "This clan sent no invites" });
+        return res
+          .status(404)
+          .json({ Error: "No invites to that clan found", Success: null });
 
       const invite = invites.find(
         (inv) => inv.InvitedEntity.Key.Id === entityKey.Id,
       );
       if (!invite)
-        return res.status(404).json({ Error: "Player has no invites" });
+        return res
+          .status(404)
+          .json({ Error: "Player has no invites", Success: null });
 
       clan.Members[1].Members.push({
         Key: entityKey,
@@ -129,10 +140,13 @@ Router.post("/acceptInvite", (req, res) => {
         if (err)
           return res
             .status(404)
-            .json({ Error: "Wasn't able to store the new clan object" });
+            .json({ Error: "Not able to save", Success: null });
 
         InviteSchema.deleteOne(invite.toObject(), (err, result) => {
-          if (err) return console.error(err);
+          if (err)
+            return res
+              .status(404)
+              .json({ Error: "Not able to delete invite", Success: null });
         });
         console.log("Player got invited");
         return res.status(201).json("Success");
@@ -148,19 +162,26 @@ Router.post("/acceptApp", (req, res) => {
 
   ClanSchema.findOne({ Group: groupData }, (err, clan) => {
     if (err)
-      return res.status(404).json({ Error: "No clan with that Id found" });
+      return res
+        .status(404)
+        .json({ Error: "No clan with that Id found", Success: null });
 
     ApplicationSchema.find({ Group: groupData }, (err, apps) => {
-      if (err) return res.status(404).json({ Error: "This clan has no apps" });
+      if (err)
+        return res
+          .status(404)
+          .json({ Error: "This clan has no apps", Success: null });
 
       if (apps.length <= 0)
-        return res.status(404).json({ Error: "This clan received no apps" });
+        return res
+          .status(404)
+          .json({ Error: "This clan received no apps", Success: null });
 
       const app = apps.find((a) => a.Entity.Key.Id === entityKey.Id);
       if (!app)
         return res
           .status(404)
-          .json({ Error: "This user didn't create any apps" });
+          .json({ Error: "Not able to save", Success: null });
 
       clan.Members[1].Members.push({
         Key: entityKey,
@@ -174,7 +195,10 @@ Router.post("/acceptApp", (req, res) => {
             .json({ Error: "Wasn't able to store the clan" });
 
         ApplicationSchema.deleteOne(app.toObject(), (err, result) => {
-          if (err) return console.error(err);
+          if (err)
+            return res
+              .status(404)
+              .json({ Error: "Not able to delete application", Success: null });
         });
         console.log("Player added to clan");
         return res.status(201).json("Success");
@@ -187,12 +211,19 @@ Router.post("/leaveClan", (req, res) => {});
 
 Router.get("/getClans", (req, res) => {
   ClanSchema.find({}, (err, clans) => {
-    if (err) return res.status(404).json({ Error: "Couldn't retrieve clans" });
+    if (err)
+      return res
+        .status(404)
+        .json({ Error: "Couldn't retrieve clans", Success: null });
 
     if (!clans.length)
-      return res.status(404).json({ Error: "Couldn't get clans" });
+      return res
+        .status(404)
+        .json({ Error: "Couldn't get clans", Success: null });
 
-    return res.status(201).json(clans.map((c) => c.toObject()));
+    return res
+      .status(201)
+      .json({ Error: null, Success: clans.map((c) => c.toObject()) });
   });
 });
 
@@ -200,11 +231,17 @@ Router.get("/getClan", (req, res) => {
   ClanSchema.findOne(
     { GroupName: JSON.parse(req.headers["bodydata"]).Name },
     (err, group) => {
-      if (err) return res.status(404).json({ Error: "Group wasn't found" });
+      if (err)
+        return res
+          .status(404)
+          .json({ Error: "Group wasn't found", Success: null });
 
-      if (!group) return res.status(404).json({ Error: "Group doesn't exist" });
+      if (!group)
+        return res
+          .status(404)
+          .json({ Error: "Group doesn't exist", Success: null });
 
-      return res.status(201).json(group.toObject());
+      return res.status(201).json({ Error: null, Success: group.toObject() });
     },
   );
 });
