fe70e3a06dfb52147e1962e2bbddd33cefd33c8a
Albin Thapaliya <wingsbroken04@gmail.com>
2024-06-23 21:18:37 +0000

    adding deleteTask command

---
+++ src/handlers/tasks/deleteTask.js
@@ -0,0 +1,31 @@
+const Devs = [
+  // add dev id here
+];
+
+module.exports = {
+  name: "deletedailytask",
+  aliases: ["ddt"],
+  permissionsRequired: ["MANAGE_GUILD"],
+  argsRequired: 1,
+
+  execute: (message, args, client) => {
+    const Id = parseInt(args[0]);
+
+    if (!Devs.includes(message.author.id))
+      return message.channel.send("You can't use this command");
+
+    if (isNaN(Id)) return message.channel.send("Id must be a number!");
+
+    client.con.query(
+      `DELETE FROM DailyTasks WHERE Id='${Id}'`,
+      (err, deleted) => {
+        if (err)
+          return message.channel.send(
+            `There is not daily task referencing this Id`,
+          );
+
+        message.channel.send(`Removed daily task with Id of ${Id}`);
+      },
+    );
+  },
+};
